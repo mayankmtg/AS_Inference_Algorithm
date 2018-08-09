6af8a59d1d4bd681f6da2c06b0043a314f609ab4
mayankmtg <mayank15056@iiitd.ac.in>
2018-08-09 06:45:54 +0000

    script changes wrt prefix files

--- knownpath/multiprefix.sh
+++ knownpath/multiprefix.sh
@@ -2,7 +2,5 @@
 
 while IFS= read -r LINE; do
 	echo "$LINE"
-	ip_addr=`nslookup $LINE | awk '{ print $2}' | tail -n 2 | head -n 1`
-	ip_prefix=`whois -h v4.whois.cymru.com "-v $ip_addr" | awk '{print $5}' | tail -n 1`
-	time python knownpath.py $ip_prefix &
+	python knownpath.py $LINE &
 done
